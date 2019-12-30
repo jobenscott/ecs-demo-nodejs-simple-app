7ce14aa0f529e740be3562cd3182f61dcc0cd998
jobenscott <aaroneveleth@gmail.com>
2019-12-30 16:47:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/status', (req, res) => res.status(200).json({status: 'ok'}))
+app.get('/', (req, res) => res.status(200).json({status: 'ok'}))
 
 app.get('/', (request, response) => {
   return response.json('well, did it work?')
