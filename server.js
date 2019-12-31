bf32c67aca1cdf6444f784c030e201c13f6be5db
jobenscott <aaroneveleth@gmail.com>
2019-12-30 18:32:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 
 app.get('/', (req, res) => res.status(200).json({status: 'ok'}))
 
-app.get('/home', (request, response) => {
-  return response.json('well, did it work?')
-})
+// app.get('/home', (request, response) => {
+//   return response.json('well, did it work?')
+// })
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
